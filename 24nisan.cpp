3479200584304c6d0f95848aa027e9b871dd2a51
Rmeyssnur <rumfth@gmail.com>
2026-04-24 09:59:08 +0300

    Add files via upload

---
+++ 24nisan.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+	/*4-STRUCT KONUSU
+	struct yemek {
+		int kalori;
+		string yemekadi;
+		double fiyat;
+	};
+	yemek mercimek;
+	yemek borek;
+	yemek doner;
+	mercimek.kalori = 150;
+	mercimek.yemekadi = "kahve";
+	mercimek.fiyat = 10.77;
+	doner.fiyat = 777;
+	doner.kalori = 100;
+	doner.yemekadi = "yumurta:";
+	cout << "fiyati:" << mercimek.fiyat << endl;
+	cout << "yemek adi:" << mercimek.yemekadi << endl;
+	cout << "yemeðin fiyati:" << doner.fiyat << endl;
+	cout << "yemegin adi:" << doner.yemekadi << endl;
+	cout << "yemegin kalorisi:" << doner.kalori << endl;*/
+
+	/* 3-STRUCT KONUSU 
+	cout << "stcrut konusu" << endl;
+	struct {
+		int kalori;
+		string yemekadi;
+		float fiyat;
+	}yemek,yemek1,yemek2;
+	yemek.kalori = 1024;
+	yemek.yemekadi = "melemen";
+	yemek.fiyat = 100;
+	yemek1.kalori = 1000;
+	yemek2.fiyat = 101010;
+	yemek2.yemekadi = "sucuklu yumurta";
+	yemek1.fiyat = 100;
+	yemek1.yemekadi = "cay";
+	yemek1.kalori = 10;
+	cout << "yemegin adi:" << yemek.yemekadi << endl;
+	cout << "yemeðin fiyati:" << yemek.fiyat << endl;
+	cout << "yemeðin kalorisi:" << yemek.kalori << endl;
+	cout << "yemeðin adi:" << yemek2.yemekadi << endl;
+	cout << "yemeðin fiyati:" << yemek2.fiyat << endl;
+	cout << "yemeðin kalorisi:" << yemek2.kalori << endl;
+	cout << "yemegin adi:" << yemek1.yemekadi << endl;
+	cout << "yemegin fiyati:" << yemek1.fiyat << endl;
+	cout << "yemegin kalorisi:" << yemek1.kalori << endl;*/
+
+
+
+
+	/*2-STRUCT KONUSU :)
+	cout << "stcrut konusu" << endl;
+	struct {
+		int kalori;
+		string yemekadi;
+		float fiyat;
+	}yemek;
+	yemek.kalori = 1024;
+	yemek.yemekadi = "melemen";
+	yemek.fiyat = 100;
+	cout << "yemeðin adi:" << yemek.yemekadi << endl;
+	cout << "yemeðin fiyati:" << yemek.fiyat << endl;
+	cout << "yemeðin kalorisi:" << yemek.kalori << endl;*/
+	
+	/*1-STRUCT KONUSU*/
+   /*cout << "Hello World!\n";
+   struct {
+	   int model;
+	   string renk;
+	   string marka;
+	   char sinif;
+	   int fiyat;
+   }otomobil;
+   
+   otomobil.marka = "bwm";
+   otomobil.fiyat = 1200;
+   cout << "markasi:" << otomobil.marka << endl;
+   cout << "fiyati:" << otomobil.fiyat << endl;*/
+}
+
+
